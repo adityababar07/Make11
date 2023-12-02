493bfee19420ad9f50b549421310ecd7a2fac108
Aditya <aditya@localhost.localdomain>
2023-12-03 00:07:54 +0530

    level 1 complete

--- make11.py
+++ make11.py
@@ -31,25 +31,68 @@ cards = [[1,2,3,4,5,6,7,8,9,'K','Q','J'],
          [1,2,3,4,5,6,7,8,9,'K','Q','J'],
          [1,2,3,4,5,6,7,8,9,'K','Q','J']]
 
-
+Rounds = 0
+Score = 0
 
 # the player has five cards so let's create a list with five cards
 
 player_cards = []
 
 # player needs to have 5 random cards in hand
-
-for i in range(4):
+no_of_cards = 0
+while no_of_cards<5:
     row = random.randint(0,3)
     coloumn = random.randint(0, 11)
-    
     card = [row, coloumn]
-    player_cards.append(card)
+    if card not in player_cards:
+        no_of_cards+=1
+        player_cards.append(card)
+
 
 print(player_cards)
 
+def computer_card():
+    row = random.randint(0,3)
+    coloumn = random.randint(0, 11)
 
-while 
+    card = [row, coloumn]
+    return card
+
+def suit_check(card):
+    if card == 0:
+        suit = 'hearts'
+    elif card== 1:
+        suit = 'diamonds'
+    elif card== 2:
+        suit = 'club'
+    else :
+        suit = 'spade'
+    
+    return suit
+
+
 # Display player's cards 
+while True:
+    Rounds += 1
+    print(f'''
+        -----------------Round {Rounds}-------------------
+    ''')
+    card = computer_card()
+
+    while card in player_cards :
+        card = computer_card()
+    
+    suit = suit_check(card)
+
+    print(f'\ncomputer deals: {cards[card[0]][card[1]]} of {suit}\nCan you make it up to 11, or produce the same suit to continue the run?\n')
+    
+    print("Please choose a card from your hand to play\n")
+
+    for i in range(5):
+        print(f"{i+1}):{suit_check(player_cards[i][0])} of {cards[player_cards[i][0]][player_cards[i][1]]}")
+
+    selection = int(input("\nEnter you selection:\t"))
+
+    
 
 #End
